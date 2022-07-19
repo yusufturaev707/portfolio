8c3a6ae24de775fe04b3b49851946088de2ec4c5
yusufturaev707 <yusufturaev707@gmail.com>
2022-07-19 16:43:54 +0500

    Added my project

---
+++ config/urls.py
@@ -0,0 +1,17 @@
+from django.views.static import serve
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include, re_path
+
+urlpatterns = [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
+
+urlpatterns += [
+    path('admin/', admin.site.urls),
+    path('', include('resume.urls')),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
